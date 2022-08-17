43954dffe65485215dae6b013d05cf537709f6f9
kifuan <kifuan@foxmail.com>
2022-08-17 23:10:20 +0800

    style: __name__ == '__main__'

--- shell.py
+++ shell.py
@@ -2,16 +2,22 @@ from helang.lexer import Lexer
 from helang.parser import Parser
 from helang.exceptions import BadStatementException, BadTokenException
 
-env = dict()
-while True:
-    text = input('Speak to Saint He > ').strip()
-    if not text.endswith(';'):
-        text += ';'
-    lexer = Lexer(text)
-    parser = Parser(lexer.lex())
-    try:
-        parser.parse().evaluate(env)
-    except (BadStatementException, BadTokenException) as e:
-        print(e)
-    except:
-        print('Error! Revise Saint He\'s videos!')
+
+def shell():
+    env = dict()
+    while True:
+        text = input('Speak to Saint He > ').strip()
+        if not text.endswith(';'):
+            text += ';'
+        lexer = Lexer(text)
+        parser = Parser(lexer.lex())
+        try:
+            parser.parse().evaluate(env)
+        except (BadStatementException, BadTokenException) as e:
+            print(e)
+        except:
+            print('Error! Revise Saint He\'s videos!')
+
+
+if __name__ == '__main__':
+    shell()
