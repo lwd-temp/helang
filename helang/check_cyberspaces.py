ff76cc7a0b057a5304a8169b6549dea72740e447
kifuan <kifuan@foxmail.com>
2022-08-18 11:38:35 +0800

    feat: check whether you are in the Cyber Spaces

---
+++ helang/check_cyberspaces.py
@@ -0,0 +1,32 @@
+import requests
+import re
+import json
+
+from .exceptions import CyberNetworkException
+
+
+# The strings is got from https://pv.sohu.com/cityjson?ie=utf-8.
+_AMERICAN_REGIONS = {
+    'UNITED STATES',
+    'JAPAN',
+}
+
+
+def _get_region() -> str:
+    try:
+        req = requests.get('https://pv.sohu.com/cityjson?ie=utf-8')
+    except Exception as e:
+        raise CyberNetworkException(f'failed to request: {e}')
+    if req.status_code != 200:
+        raise CyberNetworkException(f'request failed with status code {req.status_code}')
+    info = json.loads(re.findall(r'{.+}', req.text)[0])
+    return info['cname']
+
+
+def check_cyberspaces():
+    region = _get_region()
+    print(f'You are in {region}')
+    if region in _AMERICAN_REGIONS:
+        print('Congratulations! You are in the Cyber Spaces!')
+    else:
+        print('What a pity! It seems that you are not in the Cyber Spaces')
