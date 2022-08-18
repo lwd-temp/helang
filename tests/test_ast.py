bb7db7447796a6ee6d0ec0432f9c1a94257495b1
kifuan <kifuan@foxmail.com>
2022-08-18 17:04:34 +0800

    test: for expand method of ArithmeticAST

--- tests/test_ast.py
+++ tests/test_ast.py
@@ -1,4 +1,4 @@
-from helang.he_ast import VarExprAST, U8GetAST, U8SetAST
+from helang.he_ast import VarExprAST, U8GetAST, U8SetAST, ArithmeticAST, ArithmeticOperator, OrU8InitAST
 from helang.u8 import U8
 
 
@@ -22,3 +22,15 @@ def test_list_get():
 def test_list_set():
     U8SetAST(a, b, c).evaluate(env)
     assert env['a'] == [12, 2, 12, 4]
+
+
+def test_arithmetic():
+    ast = ArithmeticAST(
+        ArithmeticAST(OrU8InitAST(1), OrU8InitAST(2), ArithmeticOperator.ADD),
+        ArithmeticAST(OrU8InitAST(3), OrU8InitAST(4), ArithmeticOperator.SUB),
+        ArithmeticOperator.SUB)
+    asts, operators = ast._expand()
+    values = [i.evaluate(dict()) for i in asts]
+
+    assert values == [U8(1), U8(2), U8(3), U8(4)]
+    assert operators == [ArithmeticOperator.ADD, ArithmeticOperator.SUB, ArithmeticOperator.SUB]
