c47343a601fce8e3eb6688cfc0226cc4dc69e5cf
Tinhone <81268244+Tinhone@users.noreply.github.com>
2022-08-21 00:10:52 +0800

    feat: add a startup method (#303)

--- helang/launch.py
+++ helang/launch.py
@@ -89,10 +89,10 @@ def main():
     Main function
     """
     target = sys.argv[-1]
-    if target not in LAUNCHERS.keys():
+    while target not in LAUNCHERS.keys():
         legal_targets = ', '.join(LAUNCHERS.keys())
         print(f'Invalid launch target {target}, expected target: {legal_targets}.')
-        sys.exit(-1)
+        target = input('Enter the name of the target to start: ')
     if platform.system() != "Darwin":
         print("WARNING: It seems like you're using a non-Apple device, which is not cool!")
     LAUNCHERS[target]()
